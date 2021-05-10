9d42e833875a302c7934af92d8d745ed72fb761b
hyojin <jin33032@gmail.com>
2021-05-10 21:43:22 +0900

    Complete : part3 chapter 1

--- app/src/main/java/com/hjiee/fastcampus/part3/chapter1/Chapter1Activity.kt
+++ app/src/main/java/com/hjiee/fastcampus/part3/chapter1/Chapter1Activity.kt
@@ -1,7 +1,10 @@
 package com.hjiee.fastcampus.part3.chapter1
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.messaging.FirebaseMessaging
 import com.hjiee.fastcampus.databinding.ActivityPart3Chapter1Binding
 
 class Chapter1Activity : AppCompatActivity() {
@@ -11,5 +14,33 @@ class Chapter1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        initFirebase()
+        updateResult()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        updateResult(true)
+    }
+
+    private fun initFirebase() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                binding.tvToken.text = task.result
+            }
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateResult(isNewIntent: Boolean = false) {
+        binding.tvResult.text = (intent.getStringExtra("notificationType") ?: "앱 런처") +
+                if (isNewIntent) {
+                    "(으)로 갱신했습니다."
+                } else {
+                    "(으)로 실행했습니다."
+                }
+
     }
 }
